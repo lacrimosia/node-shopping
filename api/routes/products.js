09b80c23d32e6a47068efec73dc2d87c2aee47a6
Shatilla Prayer <prayers@unlv.nevada.edu>
2018-08-06 14:31:04 -0700

    added delete and patch routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -13,7 +13,13 @@ router.get('/', (req, res, next) => {
 		.exec()
 		.then(result => {
 			console.log(result);
-			res.status(200).json(result);
+			if(result.length >= 0){
+				res.status(200).json(result);
+			}else{
+				res.status(200).json({
+					message: "No entries found"
+				});
+			}	
 	})
 	.catch(err => {
 		console.log(err);
@@ -29,7 +35,8 @@ router.post('/', (req, res, next) => {
 	const product = new Product({
 		_id: new mongoose.Types.ObjectId(),
 		name: req.body.name,
-		price: req.body.price
+		price: req.body.price,
+		image: req.body.image
 	});
 	// store in database
 	product
@@ -60,18 +67,45 @@ router.get('/:productId', (req, res, next) => {
 
 router.patch('/:productId', (req, res, next) => {
 	const id = req.params.productId;
-	res.status(200).json({
-		message: "updated product!",
-		id: id
-	  });
+	const updateOps = {};
+	for(const ops of req.body){
+		updateOps[ops.propName] = ops.value;
+	}
+	Product
+		.update({
+			_id: id
+		}, {$set: updateOps})
+		.exec()
+		.then(results => {
+			console.log(results);
+			res.status(200).json({
+				message: "Product successfully updated"
+			});
+		})
+		.catch(err =>{
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
 });
 
 router.delete('/:productId', (req, res, next) => {
 	const id = req.params.productId;
-	res.status(200).json({
-		message: "Deleted product!",
-		id: id
-	  });
+	Product.remove({
+		_id: id
+	})
+	.exec()
+	.then(result => {
+		res.status(200).json({
+			"message": "Product successfully deleted"
+		});
+	})
+	.catch(err => {
+		res.status(500).json({
+			error: err
+		});
+	});
 });
 
 module.exports = router;
--- models/product.js
+++ models/product.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	name: String,
-	price: Number
+	price: Number,
+	image: String
 });
 
 
